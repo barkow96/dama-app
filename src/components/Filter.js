2382a32eb00aeaf35a4b2fd46881e11558e0a2c0
Bartosz Kowal <bartoszkowal96@gmail.com>
2021-06-20 15:55:40 +0200

    delete Filter.js

--- src/components/Filter.js
+++
@@ -1,9 +0,0 @@
-const Filter = ({changeFilter}) => {
-    return (
-        <div className="filter">
-            <input type="text" placeholder="Search for a person" onChange={(event) => changeFilter(event)}/>
-        </div>
-    );
-};
-
-export default Filter;
